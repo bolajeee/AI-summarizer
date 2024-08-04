8190a0045a3a66626e42eb22f59eed9c4215e39a
Ibrahim Kolabalogun <32678498+Kolabalogun@users.noreply.github.com>
2024-08-04 18:50:01 +0100

    Update article.js

--- src/services/article.js
+++ src/services/article.js
@@ -1,24 +1,27 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const rapidApiKey = import.meta.env.Vite_RAPID_API_ARTICLE_KEY;
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 export const articleApi = createApi({
   reducerPath: "articleApi",
   baseQuery: fetchBaseQuery({
-      baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
-      prepareHeaders:  (headers) => {
-          headers.set('x-rapidapi-key', rapidApiKey),
-          headers.set('x-rapidapi-host', 'article-extractor-and-summarizer.p.rapidapi.com')
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set("x-rapidapi-key", rapidApiKey),
+        headers.set(
+          "x-rapidapi-host",
+          "article-extractor-and-summarizer.p.rapidapi.com"
+        );
 
-          return headers;
-      }
+      return headers;
+    },
   }),
   endpoints: (builder) => ({
     getSummary: builder.query({
-        query: (params) => `/summarize?ur=
-        ${encodeURIComponent(params.articleUrl)}&length=3`,
+      query: (params) =>
+        `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
     }),
   }),
 });
 
-export const { useLazyGetSummaryQuery } = articleApi
+export const { useLazyGetSummaryQuery } = articleApi;
