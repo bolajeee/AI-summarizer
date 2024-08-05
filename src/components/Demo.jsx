86b3b6e6ce41d9514cbec958eb370cd3a3046068
ibrahim O. Ibrahim <ibrahimomobolaji1999@gmail.com>
2024-08-05 04:33:02 +0100

    edit

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,70 +1,119 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import { linkIcon, loader, copy, logo, tick } from "../assets";
-import { useLazyGetSummaryQuery } from '../services/article';
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
   const [article, setArticle] = useState({
-        url: "",
-        summary: ""
-  })
-  
-  const [getSummary, { error, isFetching }] = 
-    useLazyGetSummaryQuery()
-  
+    url: "",
+    summary: "",
+  });
+
+  const [allArticles, setAllArticles] = useState([]);
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("aticles")
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const { data } = await getSummary({
-            articleUrl:article.url
-    })
-    
+      articleUrl: article.url,
+    });
+
     if (data?.summary) {
       const newArticle = {
-        ...article, summary:data.summary
-      }
+        ...article,
+        summary: data.summary,
+      };
+      const updatedAllArticles = [newArticle, ...allArticles];
+
       setArticle(newArticle);
-      
-      console.log(newArticle)
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem("article", JSON.stringify(updatedAllArticles));
     }
-  }
+  };
   return (
-    <section className='mt-16 w-full max-w-xl'>
+    <section className="mt-16 w-full max-w-xl">
       {/*search*/}
-      <div className='flex flex-col w-full gap-2'>
-        <form className='relative flex justify-center items-center'
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
           onSubmit={handleSubmit}
         >
-          <img src={linkIcon}
-            alt='link_icon'
-            className='absolute left-0 my-2 ml-3 w-5'
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
           />
           <input
-            type='url'
-            placeholder='Enter a url'
+            type="url"
+            placeholder="Enter a url"
             value={article.url}
-            onChange={(e) => setArticle({
-              ...article,
-              url: e.target.value
-            })}
+            onChange={(e) =>
+              setArticle({
+                ...article,
+                url: e.target.value,
+              })
+            }
             required
-            className='url_input peer'
+            className="url_input peer"
           />
 
-          <button type='submit'
-            className='submit_btn peer-focus:border-gray-700
-            peer-focus:text-gray-element'
-          > ✔ </button>
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700
+            peer-focus:text-gray-element"
+          >
+            {" "}
+            ✔{" "}
+          </button>
         </form>
 
         {/*Browser Url history */}
-
-
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((article, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => {
+                setArticle(item);
+              }}
+              className="link_card"
+            >
+              <div className="copy_btn"
+              >
+                <img src={copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium
+              text-sm truncate
+              ">{article.url}</p>
+            </div>
+          ))}
+        </div>
       </div>
 
       {/* display result */}
+      <div className="my-10max-w-full flex justify-center
+      items-center"
+        {isFetching ? (<img src={loader} alt="loader" className="w-20 h-20 object-contain" /> 
+        ) : error ? (
+        <p>Well that wasnt supposed to happen</p>        }
+      )}
+    </div>
     </section>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
         <button
           type="button"
-          onClick={() => window.open("https://github.com/adrianhajdin")}
+          onClick={() => window.open("https://github.com/bolajeee")}
           className="black_btn"
         >
           Github
