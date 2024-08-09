49ca7748fd3c1ea21d0fff1209fdb2a3e13f93cd
ibrahim O. Ibrahim <ibrahimomobolaji1999@gmail.com>
2024-08-09 07:26:02 +0100

    edit

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { linkIcon, loader, copy, logo, tick } from "../assets";
+import { linkIcon, loader, copy, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
@@ -8,13 +8,15 @@ const Demo = () => {
     summary: "",
   });
 
+  const [copied, setCopied] = useState("second");
+
   const [allArticles, setAllArticles] = useState([]);
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
   useEffect(() => {
     const articlesFromLocalStorage = JSON.parse(
-      localStorage.getItem("aticles")
+      localStorage.getItem("articles")
     );
 
     if (articlesFromLocalStorage) {
@@ -39,12 +41,19 @@ const Demo = () => {
       setArticle(newArticle);
       setAllArticles(updatedAllArticles);
 
-      localStorage.setItem("article", JSON.stringify(updatedAllArticles));
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
   };
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
   return (
     <section className="mt-16 w-full max-w-xl">
-      {/*search*/}
+      {/* Search */}
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
@@ -68,20 +77,18 @@ const Demo = () => {
             required
             className="url_input peer"
           />
-
           <button
             type="submit"
             className="submit_btn peer-focus:border-gray-700
             peer-focus:text-gray-element"
           >
-            {" "}
-            ✔{" "}
+            ✔
           </button>
         </form>
 
-        {/*Browser Url history */}
+        {/* Browser URL history */}
         <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
-          {allArticles.map((article, index) => (
+          {allArticles.map((item, index) => (
             <div
               key={`link-${index}`}
               onClick={() => {
@@ -89,29 +96,52 @@ const Demo = () => {
               }}
               className="link_card"
             >
-              <div className="copy_btn"
-              >
-                <img src={copy}
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                <img
+                  src={copied === item.url ? tick : copy}
                   alt="copy_icon"
                   className="w-[40%] h-[40%] object-contain"
                 />
               </div>
-              <p className="flex-1 font-satoshi text-blue-700 font-medium
-              text-sm truncate
-              ">{article.url}</p>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
             </div>
           ))}
         </div>
       </div>
 
-      {/* display result */}
-      <div className="my-10max-w-full flex justify-center
-      items-center"
-        {isFetching ? (<img src={loader} alt="loader" className="w-20 h-20 object-contain" /> 
+      {/* Display result */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
         ) : error ? (
-        <p>Well that wasnt supposed to happen</p>        }
-      )}
-    </div>
+          <p
+            className="font-inter font-bold text-black
+              text-center 
+            "
+          >
+            Well, that wasn&apos;t supposed to happen...
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="font-inter font-medium text-sm text-gray-700">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 };
